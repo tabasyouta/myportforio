a1d5cda6ffba4fe9f3bb8bd9039f7b2de9df2c2f
taba syota <tbshow54@gmail.com>
2021-05-05 11:53:37 +0900

    make init

--- app/app.js
+++ app/app.js
@@ -1,13 +1,51 @@
-console.log("Hello World");
-
-(() => {
+(()=>{
+    // 定数の定義
     const $doc = document;
+   // const $top = $doc.getElementById("js-top");
     const $nav = $doc.getElementById("js-nav");
     const $tab = $doc.getElementById("js-tab");
-        $nav.addEventListener("click",function(){
-            $tab.querySelectorAll("[data-tab]");
-            $tab.style.display = "block";
-            console.log($tab);
-        });
+    const $tabItem = $tab.querySelectorAll("[data-tab]");
+    const $main = $doc.getElementById("js-main");
+    const $content = $main.querySelectorAll("[data-content]");
+
+    //初期化
+    const init = () => {
+        $content[0].style.display = "block";
+    };
+    init();
+
+    // クリックでタブを表示する
+    $nav.addEventListener("click",((e) => {
+        e.preventDefault();
+        $tab.style.display = "flex";
+    }))
+
+    // クリックでクラスリセットとACTクラスを追加する
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const $this =e.target;
+        const targetVar = $this.dataset.tab;
+
+        let index = 0;
+        while(index < $tabItem.length){
+            $content[index].style.display ="none";
+            $tabItem[index].classList.remove("is-active");
+            index++;
+            console.log($tabItem);
+        };
+
+        $nav.querySelectorAll('[data-content="' + targetVar + '"]')[0].style.display = "block";
+        $tab[targetVar].classList.add("is-active");
+        console.log("$tab[targetVar].classList",$tab[targetVar].classList);
+
+    };
+
+        let index = 0;
+        while(index < $tabItem.length){
+            $tabItem[index].addEventListener("click", (e) => handleClick(e));
+            index++;
+        }
+    
 
 })();
